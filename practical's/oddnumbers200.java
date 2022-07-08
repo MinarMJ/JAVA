b49b5dbcbe7b37bf6d073e6133eaa235c551377f
MinarMJ <107423941+MinarMJ@users.noreply.github.com>
2022-07-08 10:30:57 +0530

    Add files via upload

---
+++ practical's/oddnumbers200.java
@@ -0,0 +1,17 @@
+class oddnumbers200{
+    public static void main(String[] args) {
+        calculator();
+    }
+
+    static void calculator(){
+        for(int i = 0 ; i < 20000000 ; i++){
+            if (i % 2 != 0 ){
+                System.out.println(i);
+
+            }
+
+
+       }
+
+    }
+}
